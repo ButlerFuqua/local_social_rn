25ee4199954e3c4338072d36c6901b47da3f2b3e
Butler Fuqua <butlerfuqua@Butlers-MBP-2.lan>
2022-05-25 14:46:10 -1000

    Remove Rdux counter example

--- store.tsx
+++ store.tsx
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './src/features/counter/counterSlice'
 import userReducer from './src/features/user/userSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     user: userReducer,
   },
 })
